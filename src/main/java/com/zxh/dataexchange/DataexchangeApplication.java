9e42c097b000101f4a19bf4d21e1bede42dcd55e
2461664778 <2461664778@qq.com>
2019-09-19 15:05:02 +0800

    新建项目

---
+++ src/main/java/com/zxh/dataexchange/DataexchangeApplication.java
@@ -0,0 +1,13 @@
+package com.zxh.dataexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataexchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataexchangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zxh/dataexchange/DataexchangeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zxh.dataexchange;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DataexchangeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
